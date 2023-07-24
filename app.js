36d895481d812d5286fd8c90269b6c74556125b4
steven9587 <32476236+steven9587@users.noreply.github.com>
2023-07-24 21:14:22 +0800

    controller routes拆開

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+const router = require('./router');
 
 app.listen(2222, () => {
     console.log('Server is running on port 2222');
@@ -9,10 +9,10 @@ app.listen(2222, () => {
 app.set('view engine', 'ejs');
 app.use("/sources", express.static(`${__dirname}/node_modules`));
 app.use("/assets", express.static(`${__dirname}/public`));
-
-app.get('/', function(req, res){
-    res.render("index");
-});
+app.use('/', router.router);
+// app.get('/', function(req, res){
+//     res.render("index");
+// });
 
 app.get("/job/:type", (req, res)=>{
     res.render("job", {TYPE: req.params.type})
